7c7ae6ea334386fe41e7498ef81f3bdcfe9c99b7
Victor Izawa <victorsjc@gmail.com>
2019-05-14 23:40:52 -0300

    Cleanup

--- src/main/java/br/com/itau/integrador/dataprev/dto/StatusOcorrencia.java
+++
@@ -1,6 +0,0 @@
-package br.com.itau.integrador.dataprev.dto;
-
-public enum StatusOcorrencia {
-
-	ABERTO, EM_ANALISE, CONCLUIDO
-}
--- src/main/java/br/com/thinkidea/integrador/dataprev/dto/StatusOcorrencia.java
+++
@@ -1,6 +0,0 @@
-package br.com.thinkidea.integrador.dataprev.dto;
-
-public enum StatusOcorrencia {
-
-	ABERTO, EM_ANALISE, CONCLUIDO
-}
